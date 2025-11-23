df87e4514cddabbcf99ea5b6be11f5045340132a
Mzigi <smrengen@gmail.com>
2025-11-23 16:42:11 +0100

    Changed iframe and container style so editor always fits

--- public/inject.js
+++ public/inject.js
@@ -8,5 +8,8 @@ iframe.src = chrome.runtime.getURL("/index.html")
 iframe.style.width = "100%"
 iframe.style.aspectRatio = "16/9"
 iframe.style.borderStyle = "hidden"
+iframe.style.maxHeight = "80vh"
 contentElement.appendChild(iframe)
+contentElement.style.paddingTop = "0"
+contentElement.style.marginTop = "0"
 document.getElementsByTagName("body")[0].style.overflow = "hidden"
