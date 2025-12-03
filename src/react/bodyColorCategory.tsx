1eaa9a59345e3aa621b5a0184783964873dcee2e
Mzigi <smrengen@gmail.com>
2025-12-03 19:06:07 +0100

    Basic skin color select working

--- src/react/bodyColorCategory.tsx
+++ src/react/bodyColorCategory.tsx
@@ -2,47 +2,10 @@ import { useContext, useState } from "react";
 import { BodyColor3s, BodyColors, type Outfit } from "../code/avatar/outfit";
 import { OutfitContext } from "./context/outfit-context";
 import SelectInput from "./selectInput";
+import { FullBodyColors, RegularBodyColors } from "../code/avatar/constant";
+import { hexToRgb } from "../code/misc/misc";
 
-/*
-console.log(e, setOutfit)
-        const newOutfit = outfit.clone()
-
-        let newBodyColors = undefined
-        if (newOutfit.bodyColors.colorType === "BrickColor") {
-            newBodyColors = (newOutfit.bodyColors as BodyColors).toColor3()
-        } else {
-            newBodyColors = outfit.bodyColors.clone() as BodyColor3s
-        }
-
-        switch (partName) {
-            case "Head":
-                newBodyColors.headColor3 = "FFFFFF"
-                _setOutfit(newOutfit)
-                break
-            case "RightArm":
-                newBodyColors.rightArmColor3 = "FFFFFF"
-                _setOutfit(newOutfit)
-                break
-            case "LeftArm":
-                newBodyColors.leftArmColor3 = "FFFFFF"
-                _setOutfit(newOutfit)
-                break
-            case "Torso":
-                newBodyColors.torsoColor3 = "FFFFFF"
-                _setOutfit(newOutfit)
-                break
-            case "RightLeg":
-                newBodyColors.rightLegColor3 = "FFFFFF"
-                _setOutfit(newOutfit)
-                break
-            case "LeftLeg":
-                newBodyColors.leftLegColor3 = "FFFFFF"
-                _setOutfit(newOutfit)
-                break
-        }
-*/
-
-function BodyPartSelect({className, partName, currentPartNames, setCurrentPartNames, outfit}: {className: string, partName: string, currentPartNames: string[], setCurrentPartNames: (a: string[]) => void, outfit: Outfit}): React.JSX.Element {
+function partNameToHexName(partName: string) {
     let hexName: "headColor" | "torsoColor" | "rightArmColor" | "leftArmColor" | "rightLegColor" | "leftLegColor" = "headColor"
     switch (partName) {
         case "Head":
@@ -64,17 +27,29 @@ function BodyPartSelect({className, partName, currentPartNames, setCurrentPartNa
             hexName = "leftLegColor"
             break
     }
+
+    return hexName
+}
+
+function BodyPartSelect({className, partName, currentPartNames, setCurrentPartNames, outfit}: {className: string, partName: string, currentPartNames: string[], setCurrentPartNames: (a: string[]) => void, outfit: Outfit}): React.JSX.Element {
+    const hexName = partNameToHexName(partName)
     
+    const colorAsRgb = hexToRgb(outfit.bodyColors.toHexJson()[hexName]) || {r:0,g:0,b:0}
+    const averageBrightness = (colorAsRgb.r + colorAsRgb.g + colorAsRgb.b) / 3
+
+    const outlineColor = averageBrightness > 0.8 ? "#666" : "#fff"
+
     return <button className={`${className}${currentPartNames.includes(partName) ? " bodycolor bodycolor-active": " bodycolor"}`} onClick={() => {
-        const newCurrentPartNames = [...currentPartNames]
+        /*const newCurrentPartNames = [...currentPartNames]
         if (newCurrentPartNames.includes(partName)) {
             newCurrentPartNames.splice(newCurrentPartNames.indexOf(partName),1)
         } else {
             newCurrentPartNames.push(partName)
-        }
-        console.log(setCurrentPartNames)
+        }*/
+        //console.log(setCurrentPartNames)
         //setCurrentPartNames(newCurrentPartNames)
-    }} style={{backgroundColor: "#" + outfit.bodyColors.toHexJson()[hexName]}}></button>
+        setCurrentPartNames([partName])
+    }} style={{backgroundColor: "#" + outfit.bodyColors.toHexJson()[hexName], "--box-shadow-color": outlineColor} as React.CSSProperties}></button>
 }
 
 const AllPartNames = ["Head", "Torso", "RightArm", "LeftArm", "RightLeg", "LeftLeg"]
@@ -82,11 +57,142 @@ const AllPartNames = ["Head", "Torso", "RightArm", "LeftArm", "RightLeg", "LeftL
 export default function BodyColorCategory({setOutfit, _setOutfit}: {setOutfit: (a: Outfit) => void, _setOutfit: (a: Outfit) => void}): React.JSX.Element {
     const outfit = useContext(OutfitContext)
 
-    const [partNames, setPartNames] = useState<string[]>(AllPartNames)
-    const [selectPart, setSelectPart] = useState<string>("All")
+    const [partNames, _setPartNames] = useState<string[]>(AllPartNames)
+    const [selectPart, _setSelectPart] = useState<string>("All")
+
+    function setSelectPart(val: string) {
+        switch (val) {
+            case "All":
+                _setPartNames(AllPartNames)
+                break
+            case "Head":
+                _setPartNames(["Head"])
+                break
+            case "Torso":
+                _setPartNames(["Torso"])
+                break
+            case "Right Arm":
+                _setPartNames(["RightArm"])
+                break
+            case "Left Arm":
+                _setPartNames(["LeftArm"])
+                break
+            case "Right Leg":
+                _setPartNames(["RightLeg"])
+                break
+            case "Left Leg":
+                _setPartNames(["LeftLeg"])
+                break
+        }
+        _setSelectPart(val)
+    }
+
+    function setPartNames(val: string[]) {
+        if (val.length > 1) {
+            return
+        }
+
+        switch (val[0]) {
+            case "Head":
+                _setSelectPart("Head")
+                break
+            case "Torso":
+                _setSelectPart("Torso")
+                break
+            case "RightArm":
+                _setSelectPart("Right Arm")
+                break
+            case "LeftArm":
+                _setSelectPart("Left Arm")
+                break
+            case "RightLeg":
+                _setSelectPart("Right Leg")
+                break
+            case "LeftLeg":
+                _setSelectPart("Left Leg")
+                break
+        }
+
+        _setPartNames(val)
+    }
+
+    function paint(color: string) {
+        console.log(setOutfit)
+
+        const newOutfit = outfit.clone()
+
+        let newBodyColors = undefined
+        if (newOutfit.bodyColors.colorType === "BrickColor") {
+            newBodyColors = (newOutfit.bodyColors as BodyColors).toColor3()
+        } else {
+            newBodyColors = outfit.bodyColors.clone() as BodyColor3s
+        }
+        newOutfit.bodyColors = newBodyColors
+
+        for (const partName of partNames) {
+            switch (partName) {
+                case "Head":
+                    newBodyColors.headColor3 = color
+                    _setOutfit(newOutfit)
+                    break
+                case "RightArm":
+                    newBodyColors.rightArmColor3 = color
+                    _setOutfit(newOutfit)
+                    break
+                case "LeftArm":
+                    newBodyColors.leftArmColor3 = color
+                    _setOutfit(newOutfit)
+                    break
+                case "Torso":
+                    newBodyColors.torsoColor3 = color
+                    _setOutfit(newOutfit)
+                    break
+                case "RightLeg":
+                    newBodyColors.rightLegColor3 = color
+                    _setOutfit(newOutfit)
+                    break
+                case "LeftLeg":
+                    newBodyColors.leftLegColor3 = color
+                    _setOutfit(newOutfit)
+                    break
+            }
+        }
+    }
+
+    //get currentcolor
+    const currentColors: string[] = []
+    let allColorsSame = true
+
+    //get all current colors
+    for (const partName of partNames) {
+        const hexName = partNameToHexName(partName)
+        const colorValue = outfit.bodyColors.toHexJson()[hexName]
+
+        currentColors.push(colorValue)
+    }
+
+    //check if all current colors are the same
+    let lastColor = null
+    for (const color of currentColors) {
+        if (!lastColor) {
+            lastColor = color
+        } else {
+            if (lastColor !== color) {
+                allColorsSame = false
+            }
+        }
+    }
+
+    //create palette
+    const bodyColors = [...RegularBodyColors]
+    for (const color of FullBodyColors) {
+        if (!bodyColors.includes(color)) {
+            bodyColors.push(color)
+        }
+    }
 
     return <div className="bodycolor-category">
-        <div className="bodypart-select">
+        <div className="bodypart-select dark-scrollbar">
             <SelectInput value={selectPart} setValue={setSelectPart} alternatives={["All", "Head", "Torso", "Right Arm", "Left Arm", "Right Leg", "Left Leg"]}/>
             <div className="bodycolor-section bodycolor-section-top">
                 <BodyPartSelect className="bodycolor-head" partName="Head" currentPartNames={partNames} setCurrentPartNames={setPartNames} outfit={outfit}/>
@@ -102,49 +208,16 @@ export default function BodyColorCategory({setOutfit, _setOutfit}: {setOutfit: (
             </div>
         </div>
         <div className="bodycolor-select">
-            <button onClick={() => {
-                console.log(setOutfit)
-
-                const newOutfit = outfit.clone()
-
-                let newBodyColors = undefined
-                if (newOutfit.bodyColors.colorType === "BrickColor") {
-                    newBodyColors = (newOutfit.bodyColors as BodyColors).toColor3()
-                } else {
-                    newBodyColors = outfit.bodyColors.clone() as BodyColor3s
-                }
-                newOutfit.bodyColors = newBodyColors
-
-                for (const partName of partNames) {
-                    switch (partName) {
-                        case "Head":
-                            newBodyColors.headColor3 = "FFFFFF"
-                            _setOutfit(newOutfit)
-                            break
-                        case "RightArm":
-                            newBodyColors.rightArmColor3 = "FFFFFF"
-                            _setOutfit(newOutfit)
-                            break
-                        case "LeftArm":
-                            newBodyColors.leftArmColor3 = "FFFFFF"
-                            _setOutfit(newOutfit)
-                            break
-                        case "Torso":
-                            newBodyColors.torsoColor3 = "FFFFFF"
-                            _setOutfit(newOutfit)
-                            break
-                        case "RightLeg":
-                            newBodyColors.rightLegColor3 = "FFFFFF"
-                            _setOutfit(newOutfit)
-                            break
-                        case "LeftLeg":
-                            newBodyColors.leftLegColor3 = "FFFFFF"
-                            _setOutfit(newOutfit)
-                            break
-                    }
-                }
-            }
-            }></button>
+            {bodyColors.map((color) => {
+                const isSelected = allColorsSame && color.toUpperCase() === currentColors[0].toUpperCase()
+                return <button className={`bodycolor`} style={{backgroundColor: "#" + color}} onClick={() => {
+                    paint(color)
+                }}>
+                    {isSelected ? <div className="bodycolor-selected">
+                        <span className="material-symbols-outlined">check_box</span>
+                    </div> : null}
+                </button>
+            })}
         </div>
     </div>
 }
--- src/react/selectInput.tsx
+++ src/react/selectInput.tsx
@@ -1,6 +1,14 @@
+import { useState } from "react"
+
 export default function SelectInput({value, setValue, alternatives}: {value: string, setValue: (a: string) => void, alternatives: string[]}): React.JSX.Element {
-    return <button className="select roboto-600">
-        <span>{value}</span><span className="material-symbols-outlined">keyboard_arrow_down</span>
+    const [open, setOpen] = useState(false)
+    
+    return <button className="select roboto-600" onClick={() => {
+        setOpen(!open)
+    }}>
+        <span>{value}</span>
+        {open ? <span className="material-symbols-outlined">keyboard_arrow_up</span> : <span className="material-symbols-outlined">keyboard_arrow_down</span>}
+        { open ?
         <ul>
             {alternatives.map((val) => {
                 return <button className="roboto-600" onClick={() => {
@@ -10,6 +18,7 @@ export default function SelectInput({value, setValue, alternatives}: {value: str
                     {val}
                 </button>
             })}
-        </ul>
+        </ul> : null
+        }
     </button>
 }
