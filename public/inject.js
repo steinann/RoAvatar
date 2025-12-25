4984596c1a37e380c7e2b4d3d123897d0d3e23ce
Mzigi <smrengen@gmail.com>
2025-12-25 16:55:20 +0100

    Quicker inject !release

--- public/inject.js
+++ public/inject.js
@@ -1,18 +1,32 @@
-let errorElement = document.body.querySelector(".request-error-page-content")
-errorElement.remove()
+let hasInjected = false
 
-let contentElement = document.getElementById("content")
+var observer = new MutationObserver(function (mutations) {
+    let errorElement = document.body.querySelector(".request-error-page-content")
+    if (errorElement) {
+        errorElement.remove()
+    }
 
-const theme = document.body.classList.contains("dark-theme") ? "dark" : "light"
+    let contentElement = document.getElementById("content")
+    if (contentElement && !hasInjected) {
+        hasInjected = true
+        
+        const theme = document.body.classList.contains("dark-theme") ? "dark" : "light"
 
-let iframe = document.createElement("iframe")
-iframe.src = chrome.runtime.getURL(`/index.html?theme=${theme}` + window.location.search.replace("?","&"))
-iframe.style.width = "100%"
-iframe.style.aspectRatio = "16/9"
-iframe.style.borderStyle = "hidden"
-iframe.style.maxHeight = "80vh"
-contentElement.appendChild(iframe)
-contentElement.style.paddingTop = "0"
-contentElement.style.marginTop = "0"
-document.getElementsByTagName("body")[0].style.overflow = "hidden"
-document.getElementsByTagName("title")[0].innerText = "RoAvatar - Roblox"
+        let iframe = document.createElement("iframe")
+        iframe.src = chrome.runtime.getURL(`/index.html?theme=${theme}` + window.location.search.replace("?","&"))
+        iframe.style.width = "100%"
+        iframe.style.aspectRatio = "16/9"
+        iframe.style.borderStyle = "hidden"
+        iframe.style.maxHeight = "80vh"
+        contentElement.appendChild(iframe)
+        contentElement.style.paddingTop = "0"
+        contentElement.style.marginTop = "0"
+        document.getElementsByTagName("body")[0].style.overflow = "hidden"
+        document.getElementsByTagName("title")[0].innerText = "RoAvatar - Roblox"
+    }
+})
+
+observer.observe(document, {
+    childList: true,
+    subtree: true
+});
