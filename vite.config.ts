291337e201d4dcfe5e3868b55d727be7190a8bb0
Mzigi <smrengen@gmail.com>
2026-02-02 19:33:38 +0100

    The rest of my last commit that i forgot to stage

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,7 +11,15 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
-    tsconfigPaths()
+    tsconfigPaths(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "thirdparty/draco/javascript/draco_decoder.js",
+          dest: ""
+        }
+      ]
+    })
   ],
   build: {
     rollupOptions: {
