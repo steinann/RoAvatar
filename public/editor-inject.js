b6ac629141bf5dba05784149797f45e354bff64d
Mzigi <smrengen@gmail.com>
2026-02-07 15:26:09 +0100

    Tutorial on how to enter RoAvatar

--- public/editor-inject.js
+++ public/editor-inject.js
@@ -1,3 +1,7 @@
+if (!window.chrome) {
+    window.chrome = window.browser
+}
+
 let hasInjected = false
 
 var observer = new MutationObserver(function (mutations) {
@@ -14,6 +18,70 @@ var observer = new MutationObserver(function (mutations) {
             aElement.href = "https://www.roblox.com/my/avatar-plus"
 
             avatarEditorHeader.insertBefore(aElement, catalogHeader)
+            avatarEditorHeader.style.position = "relative"
+            
+            chrome.storage.local.get(["hasSeenTip"]).then((result) => {
+                const hasSeen = result["hasSeenTip"]
+                
+                if (!hasSeen) {
+                    const tipDiv = document.createElement("div")
+                    tipDiv.setAttribute("style",`
+                        position: absolute;
+                        z-index: 100;
+                        background-color: white;
+                        color: black;
+                        padding: 20px;
+                        width: 277px;
+                        border-radius: 20px 0 20px 20px;
+                        display: flex;
+                        flex-direction: column;
+                        transform: translateY(-10px);
+                        border-style: solid;
+                        border-color: gray;
+                        border-width: 2px;
+                    `)
+                    
+                    const arrow = document.createElement("div")
+                    arrow.setAttribute("style", `
+                        width: 20px;
+                        height: 20px;
+                        position: absolute;
+                        background-color: white;
+                        top: -11px;
+                        right: 2px;
+                        transform: rotate(45deg);
+                        border-color: gray;
+                        border-width: 2px;
+                        border-style: solid none none solid;
+                    `)
+
+                    const text = document.createElement("span")
+                    text.innerText = "Click here to try out RoAvatar's editor"
+
+                    const button = document.createElement("button")
+                    button.setAttribute("style", `
+                        background-color: #466eff;
+                        color: white;
+                        border: none;
+                        border-radius: 20px;
+                        font-size: medium;
+                        margin-top: 3px;
+                        padding: 2px;
+                    `)
+                    button.innerText = "Okay"
+
+                    button.addEventListener("click", () => {
+                        tipDiv.remove()
+                        chrome.storage.local.set({"hasSeenTip": true})
+                    })
+
+                    tipDiv.appendChild(arrow)
+                    tipDiv.appendChild(text)
+                    tipDiv.appendChild(button)
+
+                    avatarEditorHeader.insertBefore(tipDiv, catalogHeader)
+                }
+            })
         }
     }
 })
