3aaa23bb33bed9bee60025a5ebd5ad7e664f3a77
Mzigi <smrengen@gmail.com>
2026-02-23 11:10:28 +0100

    Undo and redo shortcuts

--- src/react/undoRedo.tsx
+++ src/react/undoRedo.tsx
@@ -1,4 +1,28 @@
+import { useEffect } from "react"
+
 export default function UndoRedo({undo,redo,canUndo,canRedo}: {undo: () => void, redo: () => void, canUndo: boolean, canRedo: boolean}): React.JSX.Element {
+    useEffect(() => {
+        const keyDownListener = (e: KeyboardEvent) => {
+            if (e.ctrlKey && !e.shiftKey) {
+                if (e.key === "z") {
+                    undo()
+                } else if (e.key === "y") {
+                    redo()
+                }
+            } else if (e.ctrlKey && e.shiftKey) {
+                if (e.key === "Z") {
+                    redo()
+                }
+            }
+        }
+
+        document.addEventListener("keydown", keyDownListener)
+        
+        return () => {
+            document.removeEventListener("keydown", keyDownListener)
+        }
+    })
+    
     return <div className="undo-redo">
         <button title="Undo" className={`history-button undo${canUndo ? "" : " history-button-inactive"}`} onClick={undo}>
             <span className="material-symbols-outlined">undo</span>
