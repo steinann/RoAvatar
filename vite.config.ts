b25bd9825a66d2657ee7a9ea6a3f30a29bfca336
Mzigi <101520429+steinann@users.noreply.github.com>
2026-07-03 18:25:09 +0200

    Updated build file to use fallback draco_decoder

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, normalizePath } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
+import path from 'node:path'
+import fs from 'node:fs'
+
+const primaryPath = path.resolve(__dirname, "thirdparty/draco/javascript/draco_decoder.js")
+const fallbackPath = path.resolve(__dirname, "node_modules/roavatar-renderer/dist/draco_decoder.js")
+
+const chosenPath = fs.existsSync(primaryPath) ? primaryPath : fallbackPath
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,7 +22,7 @@ export default defineConfig({
     viteStaticCopy({
       targets: [
         {
-          src: "thirdparty/draco/javascript/draco_decoder.js",
+          src: normalizePath(chosenPath),
           dest: ""
         }
       ]
